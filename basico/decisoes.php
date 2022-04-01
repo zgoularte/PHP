f14aafede7313346e4775ca28e5988ef037570ca
goularte <zgoularte_@hotmail.com>
2022-04-01 17:13:45 -0300

    Update decisoes.php

--- basico/decisoes.php
+++ basico/decisoes.php
@@ -1,18 +1,18 @@
 <?php
 
-$idade = 15;
-$nome = 'goularte';
+$idade = 18;
+$numeroDePessoas = 2;
 
 echo "vc so pode entrar se tiver a partir de 18 anos ou" . PHP_EOL;
 echo "a partir de 16 anos acompanhado" . PHP_EOL;
 
-if ($idade >= 18 && $nome == 'goularte') {
-    echo "vc tem $idade anos" . PHP_EOL;
-    echo 'pode entrar';
+if ($idade >= 18) {
+    echo "Você tem $idade anos. Pode entrar sozinho.";
+} else if ($idade >= 16 && $numeroDePessoas > 1) {
+    echo "Você tem $idade anos e está acompanhado(a), então pode entrar.";
 } else {
-    echo "vc só tem $idade anos, vc ñ pode entrar";
+    echo "Você só tem $idade anos. Você não pode entrar";
 }
 
 echo PHP_EOL;
-
 echo "Adeus!";
