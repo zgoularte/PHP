a3488b6f13f5dca2aed3a26a3c5f14ddabc57985
goularte <zgoularte_@hotmail.com>
2022-04-25 11:02:38 -0300

    Update loop-lista.php

--- avancado/loop-lista.php
+++ avancado/loop-lista.php
@@ -1,7 +1,7 @@
 <?php
 
-$ageList = [21, 18, 50, 24, 30, 40, 69, 70];
+$listasIdade = [18, 21, 24, 30, 40, 50, 69, 70];
 
-for ($i = 0; $i < count($ageList); $i++) {
-    echo $ageList[$i] . PHP_EOL;
+for ($i = 0; $i < count($listasIdade); $i++) {
+    echo $listasIdade[$i] . PHP_EOL;
 }
